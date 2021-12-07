aed2f2a11b8ece36918aa598fdcea665b72ac187
Rob Fisher <rob.fisher@parker.com>
2021-12-07 21:41:19 +0000

    Moves microstrain_inertial_quickview to microstrain_inertial_rqt and updates submodule

--- microstrain_inertial_rqt/setup.py
+++ microstrain_inertial_rqt/setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
   
 d = generate_distutils_setup(
-    packages=['microstrain_inertial_quickview'],
-    package_dir={'': 'microstrain_inertial_quickview_common/src'},
+    packages=['microstrain_inertial_rqt'],
+    package_dir={'': 'microstrain_inertial_rqt_common/src'},
 )
 
 setup(**d)
